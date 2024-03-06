5cf8d8ad978ebe8d7698df30c55e6e3dadaf3397
claudiols1979 <claudiols1979@hotmail.com>
2024-03-05 19:22:41 -0600

    change src app code

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">My Test App</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Where all the magic happends <code>src/App.js</code>.
         </p>
       </div>
     );
